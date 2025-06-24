be90142c30fb177b54c99e582d2202686aa007d0
weilin <weilin.wang@intel.com>
2025-06-24 11:24:04 -0700

    Update tester with new platform

--- tester.py
+++ tester.py
@@ -472,7 +472,7 @@ if __name__ == '__main__':
     parser.add_argument("-np", "--no-proxy", help="don't pass proxy env vars to vllm container", action="store_true")
     parser.add_argument("-qpc", "--queries-per-concurrency", type=int, help="Number of queries to be sent for a given concurrency")
     parser.add_argument("-i", "--iterations", type=int, help="Number of iterations to run per test")
-    parser.add_argument("-p", "--platform", choices=["spr", "gnr", "g6e", "r7i-4x", "r7i-8x", "spr-dev"], help="specify test platform (SPR/GNR/G6e/R7i.4x/R7i.8x)", required=True)
+    parser.add_argument("-p", "--platform", choices=["spr", "gnr", "g6e", "r7i-4x", "r7i-8x", "gnr-4x", "gnr-4x-small", "spr-dev"], help="specify test platform (SPR/GNR/G6e/R7i.4x/R7i.8x)", required=True)
     parser.add_argument("-ms", "--model-size", choices=["s", "l", "o"], help="specify test model list (small_size_list/large_size_list/original_list)", default="o")
     parser.add_argument("-nl", "--no-launch-vllm", help="doesn't launch or stop vllm/nginx containers. Use this to run multiple tests on prior launched vllm", action="store_true")
     parser.add_argument("-m", "--model", type=str, help="Specify model (for single model execution). If -tp is not passed, display test parameters of the model and exit")
